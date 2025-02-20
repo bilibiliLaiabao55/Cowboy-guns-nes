fc20f05d75d7c09abc41295e35c79b1ea45ef07c
bilibiliLaiabao55 <bilibili_laiabao@126.com>
2025-02-20 18:52:10 +0800

    Delete compile.bat

--- compile.bat
+++
@@ -1,9 +0,0 @@
-cc65 -t nes CowboyGuns.c -o build/CowboyGuns.s --add-source 
-
-ca65 crt0.s -o build/crt0.o
-ca65 build/CowboyGuns.s -l build/labels.txt -o build/CowboyGuns.o
-ld65 -C nrom_32k_horz.cfg build/crt0.o build/CowboyGuns.o nes.lib -o build/CowboyGuns.nes
-CD build
-CowboyGuns.NES
-
-pause
